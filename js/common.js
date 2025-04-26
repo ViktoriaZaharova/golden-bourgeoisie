1e0a2a573176a8d2adb6b9d6a40ec9b5d3840a26
Viktoria Zaharova <zaharova181114@gmail.com>
2025-04-27 00:28:02 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,29 @@
+$('[name="phone"]').mask('+7 (999) 999-99-99');
+
+// sliders
+$('.certificate-slider').slick({
+  slidesToShow: 3,
+  arrows: true,
+  appendArrows: '.certificate-slider__arrows',
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+// Fancybox
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
