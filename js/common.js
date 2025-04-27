6ee86d86bf35af2f680ebe147ad488fbb273353e
Viktoria Zaharova <zaharova181114@gmail.com>
2025-04-27 23:07:49 +0300

    + 1 section

--- js/common.js
+++ js/common.js
@@ -3,13 +3,12 @@ $('[name="phone"]').mask('+7 (999) 999-99-99');
 // sliders
 $('.certificate-slider').slick({
   slidesToShow: 3,
-  arrows: true,
   appendArrows: '.certificate-slider__arrows',
   prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
   nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
   responsive: [
     {
-      breakpoint: 1200,
+      breakpoint: 992,
       settings: {
         slidesToShow: 2,
       },
@@ -20,6 +19,39 @@ $('.certificate-slider').slick({
         slidesToShow: 1,
       },
     },
+  ],
+});
+
+$('.specialists-slider').slick({
+  slidesToShow: 5,
+  appendArrows: '.specialists-slider__arrows',
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+      },
+    }
   ],
 });
 
