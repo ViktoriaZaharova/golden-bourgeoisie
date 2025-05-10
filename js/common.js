259ddcf978281ba7bdbac4ca14c84079de995d83
Viktoria Zaharova <zaharova181114@gmail.com>
2025-05-10 23:16:29 +0300

    1024

--- js/common.js
+++ js/common.js
@@ -166,6 +166,23 @@ $('.video-gallery-slider').slick({
   appendArrows: '.video-gallery-slider__arrows',
   prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
   nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      },
+    },
+  ],
 });
 
 // Fancybox
